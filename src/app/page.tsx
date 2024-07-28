18a0401a78c331ad80d580f1eb694fdb412481de
Spike <syedibrahim1388@gmail.com>
2024-07-28 13:52:23 -0400

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2> Hello World </h2>
-      <ConnectButton client={client}/>
+      <ConnectButton client={client} />
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link href="/coinbase">
+          Mint Coinbase Nft
+        </Link>
+      </button>
 
-      <p>helloss</p>
-      <p>heloooooo</p>
-      <ProductCard/>
     </main>
   );
 }
